d8bf4c58252a9f427c7a93f57fa5c72f519ef455
Sion Smith <sion.smith@gmail.com>
2019-01-21 14:38:26 +0000

    updated iam role

--- aws_iam_role_policy_attachment.tf
+++ aws_iam_role_policy_attachment.tf
@@ -13,3 +13,9 @@ resource "aws_iam_policy_attachment" "aws-config-policy" {
   roles      = ["${aws_iam_role.local_config_rules.name}"]
   policy_arn = "${aws_iam_policy.aws-config-policy.arn}"
 }
+
+resource "aws_iam_policy_attachment" "managed-policy" {
+  name       = "aws-config-managed-policy"
+  roles      = ["${aws_iam_role.local_config_rules.name}"]
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSConfigRole"
+}

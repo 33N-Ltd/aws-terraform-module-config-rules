506841427c0913a39779c0db45b89e40d7a6d824
Sion Smith <sion.smith@gmail.com>
2019-01-22 07:27:06 +0000

    removed for testing.

--- aws_iam_policy_local_config_rules.tf
+++ aws_iam_policy_local_config_rules.tf
@@ -1,6 +1,5 @@
 resource "aws_iam_policy" "config_service_delivery_permission" {
   name        = "AWS-CONFIG-${upper(var.account_name)}-POLICY"
   description = "Managed by Terraform: Allow AWS Config to delivery logs"
-  path = "/service-role/"
   policy = "${data.aws_iam_policy_document.forrole.json}"
 }

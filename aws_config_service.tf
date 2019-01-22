85246f1b73b617f7aa1a909dd3a87772a294b651
Sion Smith <sion.smith@gmail.com>
2019-01-22 15:45:38 +0000

    testing.

--- aws_config_service.tf
+++ aws_config_service.tf
@@ -5,26 +5,56 @@ module "config_bucket" {
   s3_bucket_force_destroy = "${var.s3_bucket_force_destroy}"
   common_tags             = "${var.common_tags}"
 }
+//
+//resource "aws_config_configuration_recorder_status" "recorder_status" {
+//  name       = "AWS-CONFIG-RECORDER-STATUS-${upper(var.account_name)}"
+//  is_enabled = true
+//  depends_on = ["aws_config_delivery_channel.delivery_channel"]
+//}
+//
+//resource "aws_config_delivery_channel" "delivery_channel" {
+//  name       = "AWS-CONFIG-DELIVERY-CHANNEL-${upper(var.account_name)}"
+//  s3_bucket_name = "config-rules-${data.aws_caller_identity.current.account_id}"
+//
+//  snapshot_delivery_properties = {
+//    delivery_frequency = "${var.config_delivery_frequency}"
+//  }
+//
+//  depends_on = ["aws_config_configuration_recorder.recorder"]
+//}
+//
+//resource "aws_config_configuration_recorder" "recorder" {
+//  name       = "AWS-CONFIG-RECORDER-${upper(var.account_name)}"
+//  role_arn = "${aws_iam_role.local_config_rules.arn}"
+//
+//  recording_group = {
+//    all_supported                 = true
+//    include_global_resource_types = true
+//  }
+//}
 
-resource "aws_config_configuration_recorder_status" "recorder_status" {
-  name       = "AWS-CONFIG-RECORDER-STATUS-${upper(var.account_name)}"
+/**
+ * AWS Config Service
+ */
+resource "aws_config_configuration_recorder_status" "main" {
+  name       = "aws-config"
   is_enabled = true
-  depends_on = ["aws_config_delivery_channel.delivery_channel"]
+  depends_on = ["aws_config_delivery_channel.main"]
 }
 
-resource "aws_config_delivery_channel" "delivery_channel" {
-  name       = "AWS-CONFIG-DELIVERY-CHANNEL-${upper(var.account_name)}"
+resource "aws_config_delivery_channel" "main" {
+  name           = "aws-config"
   s3_bucket_name = "config-rules-${data.aws_caller_identity.current.account_id}"
 
   snapshot_delivery_properties = {
     delivery_frequency = "${var.config_delivery_frequency}"
   }
 
-  depends_on = ["aws_config_configuration_recorder.recorder"]
+  depends_on = ["aws_config_configuration_recorder.main"]
 }
 
-resource "aws_config_configuration_recorder" "recorder" {
-  name       = "AWS-CONFIG-RECORDER-${upper(var.account_name)}"
+resource "aws_config_configuration_recorder" "main" {
+  name     = "aws-config"
   role_arn = "${aws_iam_role.local_config_rules.arn}"
 
   recording_group = {

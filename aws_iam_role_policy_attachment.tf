b07829712c818ce1eda7578a600f66eb00455cb8
Sion Smith <sion.smith@gmail.com>
2019-01-23 15:45:15 +0000

    not needed

--- aws_iam_role_policy_attachment.tf
+++ aws_iam_role_policy_attachment.tf
@@ -1,7 +1,7 @@
-resource "aws_iam_role_policy_attachment" "local_config_rules" {
-  role       = "${aws_iam_role.local_config_rules.name}"
-  policy_arn = "${aws_iam_policy.config_service_delivery_permission.arn}"
-}
+//resource "aws_iam_role_policy_attachment" "local_config_rules" {
+//  role       = "${aws_iam_role.local_config_rules.name}"
+//  policy_arn = "${aws_iam_policy.config_service_delivery_permission.arn}"
+//}
 
 resource "aws_iam_policy" "aws-config-policy" {
   name   = "aws-config-policy"

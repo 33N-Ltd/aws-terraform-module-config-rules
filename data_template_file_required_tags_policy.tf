e7a8d68c31e5ad9da31d82ab19332471c111006e
Sion Smith <sion.smith@gmail.com>
2019-01-23 16:09:44 +0000

    fixed spelling error.

--- data_template_file_required_tags_policy.tf
+++ data_template_file_required_tags_policy.tf
@@ -3,7 +3,7 @@ data "template_file" "config_required_tag_name_policy" {
 }
 
 data "template_file" "config_required_technical_tags_policy" {
-  template = "${file("${path.module}/config-policy-templates/required-tags-technial.tpl")}"
+  template = "${file("${path.module}/config-policy-templates/required-tags-technical.tpl")}"
 }
 
 data "template_file" "config_required_operational_tags_policy" {
